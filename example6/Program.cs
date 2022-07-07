f60fa2f3d7bd278e07a1efcbaa894919cf60ad9b
Alexey Nechaeff <nechaeff91@gmail.com>
2022-07-07 20:46:58 +0500

    giri

---
+++ example6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 6;
+int b = 2;
+int c = 8;
+int d = 11;
+int e = 3;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write(max);
