f88151fa63c02d28171f7753985cf437c3bc8495
Alexey Nechaeff <nechaeff91@gmail.com>
2022-07-07 20:31:52 +0500

    МАША

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ZBS");
+}
+else
+{
+   Console.WriteLine("zdarova"); 
+   Console.WriteLine(username);
+}
